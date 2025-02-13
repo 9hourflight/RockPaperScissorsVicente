8ea1d1520f9a86abf932470565e29d95009b896a
VDelarosa <vmdel@DESKTOP-QCM166L>
2025-02-12 20:12:44 -0600

    establich variables for computer and player hands make CheckValid method to check user input with switch need to add if statements for valid cases need to make variables for win count need to make bool for win conditions

--- RockPaperScissorsLizardSpock/Program.cs
+++ RockPaperScissorsLizardSpock/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string[] handOptions = { "rock", "paper", "scissors", "lizard", "spock" };
+            Random generateHand = new Random();
+            Console.WriteLine("Please enter one of the following options to begin: rock, paper, scissors, lizard, spock");
+            string? userInput = Console.ReadLine();
+            string? inputCaseCorrected = userInput.ToLower();
+            string computerChoice = handOptions[generateHand.Next(0, 5)];
+            //Console.WriteLine("The computer chose " + computerChoice]);
+            if (inputCaseCorrected != null)
+            {
+                CheckValid(inputCaseCorrected);
+            }
         }
+        static void CheckValid(string user)
+        {
+            switch (user)
+            {
+                case "rock":
+                    break;
+                case "paper":
+                    break;
+                case "scissors":
+                    break;
+                case "lizard":
+                    break;
+                case "spock":
+                    break;
+                default:
+                    Console.WriteLine("Please enter one of the following options to begin: rock, paper, scissors, lizard, spock");
+                    break;
+
+            }
+            
+        }
+        
     }
 }
